39c73c65410f8d079bc6556da1836516af9bca1f
claudiut <claudiut@users.noreply.github.com>
2021-02-20 13:54:33 +0200

    Refactor comments in fetchDirectoryFulfilled reducer

--- src/app/components/filesSlice/reducers/fetchDirectoryFulfilled.ts
+++ src/app/components/filesSlice/reducers/fetchDirectoryFulfilled.ts
@@ -9,6 +9,7 @@ export default (
     (state: FilesState, action: Action): void => {
         state.isFetching = false;
 
+        // when receiving a list of directories
         if (Array.isArray(action.payload)) {
             state.data = action.payload.map(alphaSortFilesOfDir);
             return;
@@ -16,13 +17,14 @@ export default (
 
         const directory = alphaSortFilesOfDir(action.payload);
 
+        // add a directory and remove the unnecessary dirs, in case a parent dir gets selected
         if (action.meta.arg.sliceChildren !== false) {
-            // remove the unnecessary dirs when we chose a parent dir
             state.data = state.data.slice(0, directory.depth);
             state.data.push(directory);
             return;
         }
 
+        // overwrite/refresh a specific directory (e.g.: when after uploading a file)
         const dirIndex = state.data.findIndex(
             ({ parentPath }: Directory) => parentPath === directory.parentPath,
         );
